5968582ae36ff98b47d92d5ef35e7968f2e5231e
ankit825 <83643336+ankit825@users.noreply.github.com>
2021-07-20 20:38:06 +0530

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,12 @@
 
 
 const env = {
-  database: 'postgres',
-  username: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
+  database: 'd71mnjma6jqi1p
+',
+  username: 'pmkyihhovmcbjr',
+  password: 'e2729cc89363fabeb55b2e13313e6c08668d315e508bd25136e6c21278384ccc',
+  host: 'ec2-52-202-152-4.compute-1.amazonaws.com
+',
   dialect: 'postgres',
   pool: {
     max: 5,
